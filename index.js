32dac393077a4102e34cde4888b29a052c122b6a
gzy <gaozhanyong@users.noreply.github.com>
2025-08-01 12:21:51 +0800

    fix issue

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const RoomComponent = require('./lib/components/roomComponent')
-
-module.exports = function (app, opts) {
-  const component = new RoomComponent(app, opts)
-  // 将组件实例注册到 app 上下文，使其生命周期由 pomelo 管理
-  app.set('roomComponent', component, true)
-  return component
+module.exports = {
+  components: __dirname + '/lib/components/'
 }
